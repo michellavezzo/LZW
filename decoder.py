226014603d2a0ff4d3c3aa481dc1ed8c41a202d5
Michel Lavezzo <michellavezzo@gmail.com>
2023-05-08 19:53:50 -0300

    code cleaning

--- decoder.py
+++ decoder.py
@@ -1,19 +1,17 @@
 import struct
 
 
-
 k = input('Digite o valor de K: ')
 maximum_table_size = pow(2, int(k))
 
 dictionary_size = 256
 
-dictionary =  {i: chr(i) for i in range(dictionary_size)} #{chr(i): i for i in range(dictionary_size)}
+dictionary =  {i: chr(i) for i in range(dictionary_size)}
 decompressed_data = ""
 string = ""
 decoded = ""
 next_code = 256
-file_type = ''
-
+# file_type = 'txt' # line40 - duplicated to be easy to debug
 
 # open the binary file in read mode
 with open('binary_data.bin', 'rb') as file:
@@ -23,50 +21,32 @@ with open('binary_data.bin', 'rb') as file:
 # unpack the binary data into an array of integers
 int_array = list(struct.unpack('>' + 'i'*(len(binary_data)//4), binary_data))
 
-# print(int_array)
-
-# lzw decompress algorithm
-# for code in int_array:
-#     if not (code in dictionary):
-#         dictionary[code] = string + (string[0])    
-#         # print(dictionary[code])
-#     decompressed_data += dictionary[code]
-#     if (len(string) == 0):
-#         dictionary[next_code] = string + (dictionary[code][0])
-#         next_code += 1
-#     string = dictionary[code]
-
-
 for i, code in enumerate(int_array):
     if i == 0:
         string = dictionary[code] 
         decoded = decoded+string
     else:
-        if (code not in dictionary):
-            # dictionary[code] = string + (string[0])    
+        if (code not in dictionary): 
             new_string = string + (string[0])
         else:
             new_string = dictionary[code]
-            # print(dictionary[code])
         decoded += new_string
         if (len(dictionary) <= maximum_table_size):
             dictionary[next_code] = string + (new_string[0])
             next_code += 1
         string = new_string
 
-
+file_type = 'txt' ## duplicated to be easy to debug
+## choose the write type based on the file type
+if (file_type == 'txt'):
+    writeType = 'w' 
+else:
+    writeType = 'wb'
 # create a file to write the decompressed data
-with open('decompressed_data.mp4', 'wb') as file:
-    
+with open('decompressed_data.mp4', writeType) as file:  
     # write the decompressed data to the file
     if (file_type == 'txt'):
         file.write(decoded.encode('latin-1').decode('utf-8'))
     else:
-        #  file.write mp4 file
+        # Write media file
         file.write(bytearray(decoded.encode('latin-1')))
-
-
-
-
-# print the array of integers
-# print(int_array)
